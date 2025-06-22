7e393364e48b86660df3a7722b5db5f063610484
MatheusNevs <matheusnf62@gmail.com>
2025-06-21 23:07:00 -0300

    feat: buzzer volume and turn on/off features

--- lib/buzzer.h
+++ lib/buzzer.h
@@ -3,5 +3,8 @@
 
 void init_buzzer(void);
 void bip_by_category(unsigned int category);
+void lower_volume(void);
+void upper_volume(void);
+void lock_buzzer_off();
 
 #endif
--- source/buzzer.c
+++ source/buzzer.c
@@ -1,29 +1,72 @@
 #include "../lib/buzzer.h"
 #include "lib/utils.h"
 #include <msp430f5529.h>
+#include <stdint.h>
 
 #define BUZZER_FREQ 3000   // 3 kHz
 #define SMCLK_FREQ 1000000 // 1 MHz
+#define VOLUME_STEPS 5
 
-void init_buzzer(void) {
-  P1DIR |= BIT2; // P1.2 como saída
-  P1SEL |= BIT2; // TA0.1 função PWM
+volatile uint8_t buzzer_off_lock;
+volatile uint8_t buzzer_step;
 
-  TA0CCTL1 = OUTMOD_0; // Reset/Set
+static void update_duty(void)
+{
+  TA0CCR1 = (TA0CCR0 * 0.7 * buzzer_step) / VOLUME_STEPS;
+}
+
+void init_buzzer(void)
+{
+  buzzer_off_lock = 0;
+  buzzer_step = VOLUME_STEPS / 2;
+
+  P1DIR |= BIT2;
+  P1SEL |= BIT2;
+
+  TA0CCTL1 = OUTMOD_0;
   TA0CCR0 = (SMCLK_FREQ / BUZZER_FREQ) - 1;
-  TA0CCR1 = TA0CCR0 / 5;    // 50% duty cycle
-  TA0CTL = TASSEL_2 + MC_1; // SMCLK, modo up
+  update_duty();
+  TA0CTL = TASSEL_2 | MC_1;
 }
 
-void buzzer_on(void) {
-  TA0CCTL1 = OUTMOD_7; // PWM ativo
+void buzzer_on(void) { TA0CCTL1 = OUTMOD_7; }
+void buzzer_off(void) { TA0CCTL1 = OUTMOD_0; }
+
+void lower_volume(void)
+{
+  if (buzzer_step > 0)
+  {
+    --buzzer_step;
+    update_duty();
+  }
 }
 
-void buzzer_off(void) {
-  TA0CCTL1 = OUTMOD_0; // Força LOW
+void upper_volume(void)
+{
+  if (buzzer_step < VOLUME_STEPS)
+  {
+    ++buzzer_step;
+    update_duty();
+  }
 }
 
-void bip_by_category(unsigned int category) {
+void lock_buzzer_off(void)
+{
+  buzzer_off_lock = !buzzer_off_lock;
+  if (buzzer_off_lock)
+    buzzer_off();
+  else
+    buzzer_on();
+}
+
+void bip_by_category(unsigned int category)
+{
+  if (buzzer_off_lock)
+  {
+    buzzer_off();
+    return;
+  }
+
   static unsigned int state = 0;
   static unsigned int previous_category = 0;
   static unsigned int reference_instant = 0;
@@ -32,29 +75,33 @@ void bip_by_category(unsigned int category) {
   unsigned int past_time =
       calculate_delta_cycles(now_instant, reference_instant);
 
-  if (category == 0) {
+  if (category == 0)
+  {
     buzzer_off();
     state = 0;
     return;
   }
-
-  if (category != previous_category) {
+  if (category != previous_category)
+  {
     state = 0;
     reference_instant = now_instant;
     previous_category = category;
   }
 
-  switch (state) {
+  switch (state)
+  {
   case 0:
     buzzer_on();
     reference_instant = now_instant;
     state = 1;
     break;
   case 1:
-    if (past_time >= 9828) {
+    if (past_time >= 9828 / 8)
+    {
       if (category == 4)
         state = 0;
-      else {
+      else
+      {
         buzzer_off();
         reference_instant = now_instant;
         state = 2;
@@ -62,9 +109,9 @@ void bip_by_category(unsigned int category) {
     }
     break;
   case 2:
-    if ((category == 3 && past_time >= 6553) ||
-        (category == 2 && past_time >= 19660) ||
-        (category == 1 && past_time >= 32768))
+    if ((category == 3 && past_time >= 6553 / 8) ||
+        (category == 2 && past_time >= 19660 / 8) ||
+        (category == 1 && past_time >= 32768 / 8))
       state = 0;
     break;
   }
